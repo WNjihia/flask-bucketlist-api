69ff1d61c6916f9a29f6c3c5f53375e96dfd721f
WNjihia <dorisn172@gmail.com>
2017-06-13 10:26:15 +0300

    [Feature #146863357] create database manually

--- manage.py
+++ manage.py
@@ -1,15 +1,24 @@
 """manage.py."""
 import os
-from flask_script import Manager
+from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
+# from app import models
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+
+# manually create_db
+@manager.command
+def create_db():
+    """Create db from models."""
+    db.create_all()
+    print ('Database created!')
+
 
 if __name__ == '__main__':
     manager.run()
