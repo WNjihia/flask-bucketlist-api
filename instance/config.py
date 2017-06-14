d88f70721a2706288a8fda4b1a6c280d3abfc544
WNjihia <dorisn172@gmail.com>
2017-06-14 12:42:31 +0300

    [Feature #146865123] change test database name

--- instance/config.py
+++ instance/config.py
@@ -19,7 +19,7 @@ class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
 
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/testdb'
     DEBUG = True
 
 
