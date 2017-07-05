68a7e7bd05053dd40978ea9f8530e7f201b2ca8d
WNjihia <dorisn172@gmail.com>
2017-07-05 16:01:52 +0300

    [Feature #146867225] add create item functionality

--- bucketlist/api/views.py
+++ bucketlist/api/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, make_response, jsonify, request
 from flask.views import MethodView
 
-from bucketlist.models import Bucketlist, User
+from bucketlist.models import Bucketlist, User, Item
 
 api_blueprint = Blueprint('api', __name__)
 
@@ -156,7 +156,58 @@ class Bucketlist_View(MethodView):
             return make_response(jsonify(response)), 401
 
 
-add_bucket_view = Bucketlist_View.as_view('addbucket_api')
+class Items_View(MethodView):
+    def post(self, bucketlist_id):
+        # get the auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            user_id = User.decode_auth_token(auth_token)
+            post_data = request.get_json()
+
+            # check if bucketlist exists
+            bucketlist = Bucketlist.query.filter_by(id=bucketlist_id, creator_id=user_id).first()
+            if not bucketlist:
+                response = {
+                            'status': 'fail',
+                            'message': 'Bucketlist not found!'
+                            }
+                return make_response(jsonify(response)), 404
+
+            duplicate_item = Item.query.filter_by(item_name=post_data.get('name'),
+                                                  bucketlist_id=bucketlist_id).first()
+            if duplicate_item:
+                response = {
+                            'status': 'fail',
+                            'message': 'Item alredy exists!'
+                            }
+                return make_response(jsonify(response)), 401
+
+            new_item = Item(
+                            item_name=post_data.get('name'),
+                            description=post_data.get('description'),
+                            bucketlist_id=bucketlist_id
+                            )
+            new_item.save()
+            response = {
+                        'status': 'success',
+                        'message': 'Item {} has been added'
+                        .format(post_data.get('name'))
+                        }
+            return make_response(jsonify(response)), 201
+        else:
+            response = {
+                        'status': 'fail',
+                        'message': 'Please provide a valid auth token!'
+                        }
+            return make_response(jsonify(response)), 401
+
+
+add_bucket_view = Bucketlist_View.as_view('add_bucket_api')
+add_item_view = Items_View.as_view('add_item_view')
 
 # add rules for API endpoints
 api_blueprint.add_url_rule(
@@ -166,7 +217,19 @@ api_blueprint.add_url_rule(
 )
 
 api_blueprint.add_url_rule(
-    '/api/v1/bucketlists/<int:id>',
+    '/api/v1/bucketlists/<int:id>/',
     view_func=add_bucket_view,
+    methods=['GET', 'PUT', 'DELETE']
+)
+
+api_blueprint.add_url_rule(
+    '/api/v1/bucketlists/<int:bucketlist_id>/items/',
+    view_func=add_item_view,
+    methods=['POST', 'GET']
+)
+
+api_blueprint.add_url_rule(
+    '/api/v1/bucketlists/<int:bucketlists_id>/<int:items_id>/',
+    view_func=add_item_view,
     methods=['GET', 'PUT', 'DELETE']
 )
